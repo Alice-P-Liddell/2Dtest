b297bf1f0d57e3359fb8dad22e47cc9853ed98ed
Daehan Lee <alice.p.liddell0v0@gmail.com>
2019-11-20 17:15:40 +0900

    View
    Move Objects

---
+++ Assets/Scripts/Scroll.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroll : MonoBehaviour
+{
+    Transform tr;
+    public float minX;
+    public float maxX;
+    public float speed;
+
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tr.Translate(-1 * Time.deltaTime * speed, 0, 0);
+
+        if (tr.position.x <= minX)
+        {
+            tr.Translate(maxX * 2, 0, 0, Space.World);
+        }
+    }
+}
